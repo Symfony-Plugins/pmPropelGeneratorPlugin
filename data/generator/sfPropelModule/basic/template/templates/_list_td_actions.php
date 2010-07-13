c27f33dc963be2fe9bb4119d2edd97d44dd9593f
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-07-13 12:41:12 -0500

    fixed internationalization error.
    git-svn-id: http://svn.symfony-project.com/plugins/pmPropelGeneratorPlugin/trunk@30226 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/basic/template/templates/_list_td_actions.php
+++ data/generator/sfPropelModule/basic/template/templates/_list_td_actions.php
@@ -24,7 +24,7 @@
           </li>
         [?php else: ?]
           <li class="sf_admin_action_<?php echo $params['class_suffix'] ?> disabled">
-            <?php echo $params['label'] ?>
+            <?php echo __($params['label'], array(), 'messages') ?>
           </li>
         [?php endif ?]
       [?php else: ?]
@@ -41,7 +41,7 @@
           </li>
         [?php else: ?]
           <li class="sf_admin_action_<?php echo $params['class_suffix'] ?> disabled">
-            <?php echo $params['label'] ?>
+            <?php echo __($params['label'], array(), 'messages') ?>
           </li>
         [?php endif ?]
       [?php else: ?]
