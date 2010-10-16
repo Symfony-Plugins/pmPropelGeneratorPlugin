ca85fcc281b461ec1e25661d722fac680faaef25
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-16 11:07:50 -0500

    added support for canNew conditions. updated readme.
    git-svn-id: http://svn.symfony-project.com/plugins/pmPropelGeneratorPlugin/trunk@31152 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/basic/template/lib/helper.php
+++ data/generator/sfPropelModule/basic/template/lib/helper.php
@@ -13,16 +13,42 @@ abstract class Base<?php echo ucfirst($this->getModuleName()) ?>GeneratorHelper 
   {
     return 'list' == $action ? '<?php echo $this->params['route_prefix'] ?>' : '<?php echo $this->params['route_prefix'] ?>_'.$action;
   }
-
+  
+  public function linkToNew($params)
+  {
+    $sf_user = sfContext::getInstance()->getUser();
+    
+    if (isset($params['show_when']))
+    {
+      $show_when = $params['show_when'];
+    }
+    
+    if (method_exists('<?php echo $this->getModelClass() ?>Peer', 'canNew') && !<?php echo $this->getModelClass() ?>Peer::canNew($sf_user))
+    {
+      return '<li class="sf_admin_action_new_disabled">'.__($params['label'], array(), 'sf_admin').'</li>';
+    }
+    
+    if (isset($show_when))
+    {
+      return (call_user_func(array('<?php echo $this->getModelClass() ?>Peer', $show_when))) ? parent::linkToNew($params) : '';
+    }
+    else
+    {
+      return parent::linkToNew($object);
+    }
+  }
+  
   public function linkToEdit($object, $params)
   {
+    $sf_user = sfContext::getInstance()->getUser();
+    
     $obj = $object instanceOf sfOutputEscaperObjectDecorator ? $object->getRawValue() : $object;
     if (isset($params['show_when']))
     {
       $show_when = $params['show_when'];
     }
 
-    if (method_exists($obj, 'canEdit') && !$obj->canEdit())
+    if (method_exists($obj, 'canEdit') && !$obj->canEdit($sf_user))
     {
       return '<li class="sf_admin_action_edit_disabled">'.__($params['label'], array(), 'sf_admin').'</li>';
     }
@@ -39,13 +65,15 @@ abstract class Base<?php echo ucfirst($this->getModuleName()) ?>GeneratorHelper 
 
   public function linkToDelete($object, $params)
   {
+    $sf_user = sfContext::getInstance()->getUser();
+    
     $obj = $object instanceOf sfOutputEscaperObjectDecorator ? $object->getRawValue() : $object;
     if (isset($params['show_when']))
     {
       $show_when = $params['show_when'];
     }
 
-    if (method_exists($obj, 'canDelete') && !$obj->canDelete())
+    if (method_exists($obj, 'canDelete') && !$obj->canDelete($sf_user))
     {
       return '<li class="sf_admin_action_delete_disabled">'.__($params['label'], array(), 'sf_admin').'</li>';
     }
@@ -62,13 +90,15 @@ abstract class Base<?php echo ucfirst($this->getModuleName()) ?>GeneratorHelper 
 
   public function linkToShow($object, $params)
   {
+    $sf_user = sfContext::getInstance()->getUser();
+    
     $obj = $object instanceOf sfOutputEscaperObjectDecorator ? $object->getRawValue() : $object;
     if (isset($params['show_when']))
     {
       $show_when = $params['show_when'];
     }
 
-    if (method_exists($obj, 'canShow') && !$obj->canShow())
+    if (method_exists($obj, 'canShow') && !$obj->canShow($sf_user))
     {
       return '<li class="sf_admin_action_show_disabled">'.__($params['label'], array(), 'sf_admin').'</li>';
     }
