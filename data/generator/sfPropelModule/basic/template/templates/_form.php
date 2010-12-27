e33281d140410557189fce4e076051977c8f173d
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-12-27 08:20:51 -0600

    improvents in _form, _form_tabbed and _form_folded templates.
    git-svn-id: http://svn.symfony-project.com/plugins/pmPropelGeneratorPlugin/trunk@31722 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/basic/template/templates/_form.php
+++ data/generator/sfPropelModule/basic/template/templates/_form.php
@@ -16,33 +16,15 @@
           [?php if ('NONE' != $fieldset): ?]
           <h2>[?php echo __($fieldset, array(), '<?php echo $this->getI18nCatalogue() ?>') ?]</h2>
           [?php endif; ?]
-          [?php if ($form->getWidgetSchema()->getFormFormatterName() == 'table'): ?]
-            <table>
-              <tbody>
-                [?php foreach ($field_names as $name): ?]
-                  [?php echo $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
-                [?php endforeach ?]
-              </tbody>
-            </table>
-          [?php else: ?]
-            [?php $content = "" ?]
-            [?php foreach ($field_names as $name): ?]
-              [?php $content .= $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
-            [?php endforeach ?]
-            [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => $content)) ?]
-          [?php endif ?]
+          [?php $content = "" ?]
+          [?php foreach ($field_names as $name): ?]
+            [?php $content .= $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
+          [?php endforeach ?]
+          [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => $content)) ?]
         </fieldset>
       [?php endforeach ?]
     [?php else: ?]
-      [?php if ($form->getWidgetSchema()->getFormFormatterName() == 'table'): ?]
-        <table>
-          <tbody>
-            [?php echo $form ?]
-          </tbody>
-        </table>
-      [?php else: ?]
-        [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => (string) $form)) ?]
-      [?php endif ?]
+      [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => (string) $form)) ?]
     [?php endif ?]
 
     [?php include_partial('<?php echo $this->getModuleName() ?>/form_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?]
--- data/generator/sfPropelModule/basic/template/templates/_form_tabbed.php
+++ data/generator/sfPropelModule/basic/template/templates/_form_tabbed.php
@@ -15,34 +15,16 @@
       [?php foreach (call_user_func(array($form, "get{$action}Fieldsets")) as $fieldset => $field_names): ?]
         <fieldset id="sf_fieldset_[?php echo preg_replace('/[^a-z0-9_]/', '_', strtolower($fieldset)) ?]"[?php echo ($form->getWidgetSchema()->getFormFormatterName() == 'table') ? " style=\"background: none; border: none;\"":""?]>
           <div id="tab_[?php echo $fieldset ?]" class="[?php echo ($first ? 'selected' : 'not-selected'); $first = false; ?]">
-            [?php if ($form->getWidgetSchema()->getFormFormatterName() == 'table'): ?]
-              <table>
-                <tbody>
-                  [?php foreach ($field_names as $name): ?]
-                    [?php echo $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
-                  [?php endforeach ?]
-                </tbody>
-              </table>
-            [?php else: ?]
-              [?php $content = "" ?]
-              [?php foreach ($field_names as $name): ?]
-                [?php $content .= $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
-              [?php endforeach ?]
-              [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => $content)) ?]
-            [?php endif ?]
+            [?php $content = "" ?]
+            [?php foreach ($field_names as $name): ?]
+              [?php $content .= $form[$name]->getWidget() instanceof sfWidgetFormInputHidden ? $form[$name]->render() : $form[$name]->renderRow() ?]
+            [?php endforeach ?]
+            [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => $content)) ?]
           </div>
         </fieldset>
       [?php endforeach ?]
     [?php else: ?]
-      [?php if ($form->getWidgetSchema()->getFormFormatterName() == 'table'): ?]
-        <table>
-          <tbody>
-            [?php echo $form ?]
-          </tbody>
-        </table>
-      [?php else: ?]
-        [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => (string) $form)) ?]
-      [?php endif ?]
+      [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => (string) $form)) ?]
     [?php endif ?]
 
     [?php include_partial('<?php echo $this->getModuleName() ?>/form_actions', array('<?php echo $this->getSingularName() ?>' => $<?php echo $this->getSingularName() ?>, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?]
