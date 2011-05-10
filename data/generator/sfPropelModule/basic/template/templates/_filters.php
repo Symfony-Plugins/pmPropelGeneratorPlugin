dda584baa1c14aee05c982bdf0f60cdfcf03d612
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-05-10 09:43:44 -0500

    git-svn-id: http://svn.symfony-project.com/plugins/pmPropelGeneratorPlugin/trunk@32522 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/basic/template/templates/_filters.php
+++ data/generator/sfPropelModule/basic/template/templates/_filters.php
@@ -4,7 +4,7 @@
 <div class="sf_admin_filter">
   <form action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter')) ?]" method="post">
     [?php echo link_to_function("<h2>".__('Apply filters to list', array(), 'sf_admin')."</h2>", "var div = document.getElementById('sf_admin_filter_hideable'); if (div.style.display == 'none') { div.style.display = 'block' } else { div.style.display = 'none' }")  ?]
-    <div id="sf_admin_filter_hideable" style="display: none;">
+    <div id="sf_admin_filter_hideable" [?php !count($sf_user->getAttribute('<?php echo $this->getModuleName() ?>.filters',array(),'admin_module')) and print 'style="display: none;"' ?]>
         [?php echo strtr($form->getWidgetSchema()->getFormFormatter()->getDecoratorFormat(), array("%content%" => (string) $form)) ?]
         <div class="sf_admin_form_row" style="text-align: right;">
           [?php echo link_to(__('Reset', array(), 'sf_admin'), '<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post')) ?]
