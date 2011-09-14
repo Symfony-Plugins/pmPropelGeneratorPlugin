5ccf89781096ce72f88e9575bd5895c4bb6a92fa
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-09-14 13:54:38 -0500

    se agrega la opción del show when en las acciones de los edit y new. Tener en cuenta que para mandarle el mensaje del show when usa el objeto del form, y si este no posee ninguno asociado, por ejemplo en el new, será un problema a tener en cuenta
    git-svn-id: http://svn.symfony-project.com/plugins/pmPropelGeneratorPlugin/trunk@33020 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfPropelModule/basic/template/templates/_form_actions.php
+++ data/generator/sfPropelModule/basic/template/templates/_form_actions.php
@@ -6,6 +6,7 @@
 [?php else: ?]
 <?php endif ?>
 <?php foreach ($this->configuration->getValue($action.'.actions') as $name => $params): ?>
+  <?php $show_when = isset($params['show_when'])?$params['show_when']:false; ?>
 <?php if ('_delete' == $name): ?>
   <?php echo $this->addCredentialCondition('[?php echo $helper->linkToDelete($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
 
@@ -22,15 +23,21 @@
   <?php echo $this->addCredentialCondition('[?php echo $helper->linkToSaveAndAdd($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
 
 <?php else: ?>
-  <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
-[?php if (method_exists($helper, 'linkTo<?php echo $method = ucfirst(sfInflector::camelize($name)) ?>')): ?]
-  <?php echo $this->addCredentialCondition('[?php echo $helper->linkTo'.$method.'($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
+  <?php if($show_when): ?>
+  [?php if (method_exists($form->getObject(), '<?php echo $show_when?>') && $form->getObject()-><?php echo $show_when?>($sf_user)): ?]
+  <?php endif; ?>
+      <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
+    [?php if (method_exists($helper, 'linkTo<?php echo $method = ucfirst(sfInflector::camelize($name)) ?>')): ?]
+      <?php echo $this->addCredentialCondition('[?php echo $helper->linkTo'.$method.'($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
 
-[?php else: ?]
-  <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
+    [?php else: ?]
+      <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
 
-[?php endif ?]
-  </li>
+    [?php endif ?]
+      </li>
+  <?php if($show_when): ?>
+  [?php endif ?]
+  <?php endif; ?>
 <?php endif ?>
 <?php endforeach ?>
 <?php endforeach ?>
